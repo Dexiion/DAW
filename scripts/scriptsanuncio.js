9f6b460e35087ed429258879ed7ba72caf5f486c
Juan <Juanjspm@hotmail.com>
2021-11-22 18:45:57 +0000

    anuncio carga dinamica integrada

--- scripts/scriptsanuncio.js
+++ scripts/scriptsanuncio.js
@@ -22,6 +22,11 @@ $(document).ready(function() {
     });
     createAdCard();
     createCarousel();
+    createDesc();
+    createInfoCar();
+    createImagecontacto();
+    createInfoContacto();
+    
     // console.log(objJson);
     // console.log(imagesLinks);
     // console.log(accountJson);
@@ -70,7 +75,101 @@ var createAdCard = function(){
     $('#MarcaPrecio').append(text);
 }
 
-var createCarousel =function(){
+var createDesc =  function(){
+    var data = objJson[0];
+    var desc = "<h4 class=\"box-title mt-3\">Descripcion</h4> \
+    <p > {{Descripcion}} </p>";
+    var text = "";
+    text = Mustache.render(desc, data);
+    $('#descripcion').append(text);
+
+}
+
+var createInfoContacto =  function(){
+    var data = accountJson[0];
+    var tablacontaco = " <table class=\"table table-striped table-product mt-2\"> \
+        <tbody> \
+            <tr> \
+                <td >Nombre</td> \
+                <td>{{NombreApellidos}}</td> \
+            </tr> \
+            <tr> \
+                <td>Telefono</td> \
+                <td>{{Telefono}}</td> \
+            </tr> \
+            <tr> \
+                <td>Email</td> \
+                <td><small>{{Email}}</td> \
+            </tr> \
+        </tbody> \
+    </table>";
+
+    var text = "";
+    text = Mustache.render(tablacontaco, data);
+    $('#tableVendedor').append(text);
+
+}
+
+var createImagecontacto = function() {
+    var data = accountJson[0];
+    var imagecont = "<h4 class=\"card-title text-center\"> Vendedor</h4> \
+    <div class=\"white-box text-center\"> \
+        <img src={{Imagen}} class=\"img-fluid\"> \
+    </div>";
+    var text = "";
+    text = Mustache.render(imagecont, data);
+    $('#fotocontacto').append(text);
+}
+
+
+var createInfoCar = function(){
+    var data = objJson[0];
+    var tabla = "<h4 class=\"box-title mt-lg-4\">Informacion del vehiculo</h4> \
+    <div class=\"table-responsive\"> \
+        <table class=\"table table-striped table-product \"> \
+            <tbody> \
+                <tr> \
+                    <td width=\"390\" >Fecha de matriculación</td> \
+                    <td>{{Matriculacion}}</td> \
+                </tr> \
+                <tr> \
+                    <td>Kilómetros</td> \
+                    <td>{{Kilometros}}</td> \
+                </tr> \
+                <tr> \
+                    <td>Combustible</td> \
+                    <td>{{Combustible}}</td> \
+                </tr> \
+                <tr> \
+                    <td>Localizacion</td> \
+                    <td>{{Localizacion}}</td> \
+                </tr> \
+                <tr> \
+                    <td>Potencia</td> \
+                    <td>{{Potencia}}</td> \
+                </tr> \
+                <tr> \
+                    <td>Color</td>\
+                    <td>{{Color}}</td> \
+                </tr> \
+                <tr> \
+                    <td>Puertas</td> \
+                    <td>{{Puertas}}</td> \
+                </tr> \
+                <tr> \
+                    <td>Caja de cambios</td> \
+                    <td>{{CajaDeCambios}}</td> \
+                </tr> \
+            </tbody> \
+        </table> \
+    </div>";
+
+    var text = "";
+    text = Mustache.render(tabla, data);
+    $('#infotabla').append(text);
+} 
+
+var createCarousel = function(){
     imagesLinks.forEach(image => {
         
         var firstImage = "<div class=\"carousel-item active\"> \
