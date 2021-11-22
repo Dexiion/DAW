6a9e5d0f29501ddbd45b8041e28aadfad1a751dd
Juan <Juanjspm@hotmail.com>
2021-11-22 18:09:29 +0000

    avance

--- scripts/scriptsanuncio.js
+++ scripts/scriptsanuncio.js
@@ -1,45 +1,97 @@
-var imageLink = "";
+
 var counter = 1;
-idCar = 3;
 
 
+var carID ="";
+var objJson = [];
+var imagesLinks = [];
+var accountJson = [];  
+
+$(document).ready(function() {
+    var params = getQueryParams();
+    carID = params.get("carid")
+    $.ajaxSetup({
+        async: false
+    });
+    getCars();
+    getImageLink();
+    getProfile();
+    
+    $.ajaxSetup({
+        async: true
+    });
+    createAdCard();
+    createCarousel();
+    // console.log(objJson);
+    // console.log(imagesLinks);
+    // console.log(accountJson);
+
+});
+
+var getQueryParams = function() {
+    return new URLSearchParams(window.location.search);
+}
+
+function getCars(){
+    $.getJSON('../data/cars.json', function(cars) {
+        
+        objJson=cars.filter(car => car.ID == carID)
+        
+    });
+    
+}
 
 
-function getImageLink(idCar) {
+function getImageLink() {
     $.getJSON('../data/carimages.json', function(images) {
         images.forEach(image => {
-            if(image.IDCar == idCar) {
-                imageLink = image.Link;
+            if(image.IDCar == carID) {
+                imagesLinks.push(image.Link);
             }
         });
     });
 }
 
-
-var createAdCard = function(anuncio){
-    var data = anuncio;
-
-    var firstHtmlCard = "<div class=\"carousel-item active\"> \
-                            <div class=\"col-12\"> \
-                                <div class=\"card-sl\" style=\"margin-right: 10px;\"> \
-                                    <div class=\"card-image\"> \
-                                        <img src=\"" + imageLink + "\"/> \
-                                    </div> \
-                                </div> \
-                            </div> \
-                        </div>";
-
-var text = "";
-
-if(counter == 1) {
-text = Mustache.render(firstHtmlCard, data);
-counter--;
-} else {
-text = Mustache.render(htmlCard, data);
-}
-
-$('#outstandingCarousel').append(text);
+function getProfile() {
+    $.getJSON('../data/accounts.json', function(accounts) {
+        var userID = objJson[0].IDUsuario;
+        accountJson = accounts.filter(account => account.ID == userID);
+    });
 }
 
 
+
+
+var createAdCard = function(){
+    var data = objJson[0];
+    var firstHtmlCard = "<h3 class=\"card-title text-center\"> {{Marca}} {{Modelo}}  -  {{Precio}} € </h3>";
+    var text = "";
+    text = Mustache.render(firstHtmlCard, data);
+    $('#MarcaPrecio').append(text);
+}
+
+var createCarousel =function(){
+    imagesLinks.forEach(image => {
+        
+        var firstImage = "<div class=\"carousel-item active\"> \
+            <div class=\"card-image-anuncio\"> \
+                <img src=\"" + image + "\"> \
+            </div>    \
+        </div>";
+
+        var otherImage = "<div class=\"carousel-item\"> \
+            <div class=\"card-image-anuncio\"> \
+                <img src=\"" + image + "\"> \
+            </div>    \
+        </div>";    
+        if (counter == 1){
+            $('#Imagenes').append(firstImage);
+            counter--;
+        }else{
+            $('#Imagenes').append(otherImage);
+        }
+    });
+
+    
+}
 
